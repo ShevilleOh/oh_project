5768f78555c4712c05bbf068376bf2177d84aeee
OH <ohmydaniel@gmail.com>
2020-08-20 15:25:09 +0900

    Header&NavBar

--- src/component/layout/Footer.js
+++ src/component/layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="bg-dark text-white mt-5 p-4 text-center">
-            Copyright &copy; {new Date().getFullYear()} Oh_Project
+            Copyright &copy; {new Date().getFullYear()} Control Alt Elite
 
         </footer>
     );
