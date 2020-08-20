1c2b13be4f8b5bae49fc6a8f1fe42485d5b7050e
OH <ohmydaniel@gmail.com>
2020-08-20 15:52:06 +0900

    Landing&IMG

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from "./component/layout/Navbar";
+import Landing from "./component/layout/Landing";
 import Footer from "./component/layout/Footer";
 
 function App() {
   return (
     <div className="App">
         <Navbar />
-      <h1>Home Screen</h1>
+        <Landing />
         <Footer />
     </div>
   );
