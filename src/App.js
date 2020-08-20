f1b07b6bdafc0110e965e11da6d159a2099d2103
OH <ohmydaniel@gmail.com>
2020-08-20 14:48:40 +0900

    add footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Footer from "./component/layout/Footer";
 
 function App() {
   return (
     <div className="App">
       <h1>Home Screen</h1>
+        <Footer />
     </div>
   );
 }
